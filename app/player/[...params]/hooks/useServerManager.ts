10000792613fa1abe76dae85e6c9ce7f8658fc1b
jerometecson-main <jerometecson.main@gmail.com>
2026-03-10 19:52:21 +0800

    xxx

--- app/player/[...params]/hooks/useServerManager.ts
+++ app/player/[...params]/hooks/useServerManager.ts
@@ -39,17 +39,18 @@ export function useServerManager({
 }) {
   const [servers, setServers] = useState<ServerTypes[]>([
     {
-      name: "Atlas (NEW)",
-      server: 22,
-      status: "queue",
-      desc: "Stable, may include 4K and multi-language.",
-    },
-    {
       name: "Icarus",
       server: 11,
       status: "queue",
       desc: "Fast and stable.",
     },
+    {
+      name: "Atlas (NEW)",
+      server: 22,
+      status: "queue",
+      desc: "Stable, may include 4K and multi-language.",
+    },
+
     { name: "Daedalus I", server: 0, status: "queue", desc: "Main Server" },
 
     {
